78d8653305ee1cf5b0bb89616c596d34e3d666bc
Mega17Z <planimportant@gmail.com>
2025-04-29 16:39:07 +0000

    integration de l'image

--- src/components/MainTravel.jsx
+++ src/components/MainTravel.jsx
@@ -5,10 +5,11 @@ const MainTravel = () => {
     <div className='container-fluid' style={{height: 580}}>
         <InputSearch/>
     <div className='image mt-4'>
-       <div className='d-flex justify-content-center'>
-       <h1 className='text-center'>Explorez Le Monde Avec TravelCompagny</h1>
+       <div className='d-flex justify-content-center align-items-center flex-column text-travel text-center'>
+        <h3 className='mb-3'>Explorez Le Monde Avec TravelCompagny</h3>
+        <p className='mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium molestias error? Consequuntur corrupti illo assumenda laboriosam? Rerum aliquam reiciendis atque aliquid voluptates architecto dolore quibusdam? Perferendis voluptatum neque necessitatibus.</p>
        </div>
-        </div>
+    </div>
 
     </div>
   )
