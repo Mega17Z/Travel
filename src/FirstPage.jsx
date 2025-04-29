179691c6aafe6f31f3ea09c738f719619a4793ab
Mega17Z <planimportant@gmail.com>
2025-04-29 16:10:04 +0000

    git

--- src/FirstPage.jsx
+++ src/FirstPage.jsx
@@ -1,17 +1,19 @@
 import Decouvertes from "./components/Decouvertes/decouvertes";
 // import Foot from "./components/Footer/foot";
-import DetailsVols from "./components/DetailsVols/details";
-import Foot from "./components/Footer/foot";
+// import DetailsVols from "./components/DetailsVols/details";
+// import Foot from "./components/Footer/foot";
 import Footer from "./components/Footer/footer";
 import RechercheVols from "./components/rechercheVol/rechercheVol";
 // import Navbar from "./components/Navbar";
-import InputSearch from "./components/InputSearch"
+// import InputSearch from "./components/InputSearch"
+import MainTravel from "./components/MainTravel";
 
 const MonAccueil = () => {
     return ( 
         <div>
             {/* <Navbar /> */}
-            <InputSearch />
+            {/* <InputSearch /> */}
+            <MainTravel />
             <RechercheVols />
             <Decouvertes />
             <Footer />
--- src/components/MainTravel.jsx
+++ src/components/MainTravel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import InputSearch from './InputSearch'
 const MainTravel = () => {
   return (
-    <div className='container-fluid' style={{backgroundColor: '#f1f1f1', height: 580}}>
+    <div className='container-fluid' style={{height: 580}}>
         <InputSearch/>
     <div className='image mt-4'>
        <div className='d-flex justify-content-center'>
