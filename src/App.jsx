a5cb99b4ccc6a8de63906d77733c40d15fdbf268
Mega17Z <planimportant@gmail.com>
2025-04-30 13:17:15 +0000

    change

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       <Route path='/' element={<MonAccueil />} />
       <Route path='/ListesVol' element={<LesVols />} />
       <Route path='/Reservation' element={<Reservation />} />
-      <Route path='' element={<Details />} />
+      <Route path='/AfficheDetaiils/id:' element={<Details />} />
      {/* <InputSearch/> */}
      {/* <MainTravel/> */}
      </Routes>
