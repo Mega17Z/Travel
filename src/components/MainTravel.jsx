f6774d04ccf3666517fe835ebffcd47ea3bf451a
Nafi-Badji <badjimoretalla@gmail.com>
2025-04-30 09:39:57 +0000

    add

--- src/components/MainTravel.jsx
+++ src/components/MainTravel.jsx
@@ -4,9 +4,13 @@ const MainTravel = () => {
   return (
     <div className='container-fluid' style={{backgroundColor: '#f1f1f1', height: 580}}>
         <InputSearch/>
-    <div className='image mt-4'>
-       <div className='d-flex justify-content-center'>
+    <div className='image '>
+       <div className='para mt-5'>
        <h1 className='text-center'>Explorez Le Monde Avec TravelCompagny</h1>
+       <p className='text-center'>
+        Partez à l'aventure, découvrez de nouveaux horizons et créez des souvenirs <br /> inoubliables. 
+        Votre prochain voyage commence ici.
+       </p>
        </div>
         </div>
 
