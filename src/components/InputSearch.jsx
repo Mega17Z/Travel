2064e144dee3109651de842d7a2fe67fe529a0ce
Nafi-Badji <badjimoretalla@gmail.com>
2025-05-01 00:26:04 +0000

    gap

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { data } from "react-router-dom";
 
 const InputSearch = () => {
+     
+  const [vols, setVols] = useState([])
+  const [recherche, setRecherche] = useState("")
+  const [resultFiltre, setResultFiltre] = useState([])
+
+  useEffect(() =>{
+   fetch('http://localhost:9000/vols')
+   .then((res) => {
+    if(!res.ok){
+       throw new Error('Erreur réseau')
+    }
+    return res.json()
+   }) 
+   .then((data) =>{
+    setVols(data)
+   })
+   .catch((error) =>{
+    console.error('Erreur lors du fetch des vols :' , error)
+   })
+  }, [])
+
   return (
     <div>
       <div className="container" >
@@ -9,7 +31,6 @@ const InputSearch = () => {
             className="form-control mt-4 bg-white rounded-5"
             type="search"
             placeholder="Recherche de Vols..."
-            aria-label="Search"
           />
           <i className="bi bi-search"></i>
         </form>
