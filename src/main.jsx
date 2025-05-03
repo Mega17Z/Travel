37eb7d0b7b52d1b52604ff8fe6e2594d8d6d7dec
Mega17Z <planimportant@gmail.com>
2025-05-03 22:38:36 +0000

    part details avec utilisation de useContext

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { FormProvider } from "./components/Formulaire/FormContext.jsx"
 import { AuthProvider } from './components/Formulaire/AuthContext.jsx'
+import { ReservationProvider } from './components/ReservationContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
     <FormProvider>
+    <ReservationProvider>
     <App />
+    </ReservationProvider>
     </FormProvider>
     </AuthProvider>
   </StrictMode>,
